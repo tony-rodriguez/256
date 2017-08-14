16cf9e973609ac607ae296bf0862674573e70a5b
Dominique Goncalves & Tony Rodriguez <pair+dgonca.tony-rodriguez@devbootcamp.com>
2017-08-14 10:40:10 -0500

    on game new, board is either randomly generated or you can give it a starting board

--- js/application.js
+++ js/application.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
-
+  var game = new Game();
+  console.log(game.board);
+  var game2 = new Game('0000000020000200');
+  console.log(game2.board);
 });
---
+++ js/game.js
@@ -0,0 +1,33 @@
+var sample = function(array){
+  return array[Math.floor(Math.random()*array.length)];
+}
+
+String.prototype.replaceAt=function(index, replacement) {
+    return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
+}
+
+// var strSample = function(string){
+//   return string.charAt(Math.floor(Math.random()*string.length));
+// }
+
+var generateRandomBoard = function(){
+  var zeroBoard = '0000000000000000';
+  var randomStarts = ['2', '4'];
+  for(var i = 0; i < 2; i++){
+    var index = Math.floor(Math.random()*zeroBoard.length);
+    if (zeroBoard.charAt(index) === '0') {
+     zeroBoard = zeroBoard.replaceAt(index, sample(randomStarts));
+    } else {
+      i--;
+    }
+  }
+  return zeroBoard;
+}
+
+var Game = function(board){
+  this.board = board || generateRandomBoard();
+}
+
+Game.prototype.spawnRandomTile = function(){
+
+}
