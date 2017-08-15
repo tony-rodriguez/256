c6c7f6990d140f72d4e4724c56ed7df8020abae0
Tony Rodriguez <tonyr923@gmail.com>
2017-08-14 22:37:40 -0500

    Add styling, allow game to be played by pressing buttons in browser, fixed edge case for jumping merges.

--- js/application.js
+++ js/application.js
@@ -1,6 +1,28 @@
+var reloadTableValues = function(game) {
+  for(var i = 0; i < game.board.length; i++){
+    $('#' + i).text(game.board[i]);
+  };
+}
+
 $(document).ready(function() {
   var game = new Game();
-  console.log(game.board);
-  var game2 = new Game('0000000020000200');
-  console.log(game2.board);
+  reloadTableValues(game);
+  $(document).on('keyup', function(e) {
+    if (e.keyCode === 37) {
+      game.move('left');
+      reloadTableValues(game);
+    }else if(e.keyCode === 38) {
+      game.move('up');
+      reloadTableValues(game);
+    }else if(e.keyCode === 39) {
+      game.move('right');
+      reloadTableValues(game);
+    }else if(e.keyCode === 40) {
+      game.move('down');
+      reloadTableValues(game);
+    }
+  })
+  $(".restart-button").on('click', function() {
+    location.reload();
+  })
 });
